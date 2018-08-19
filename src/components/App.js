f85285a8f7d16a857c779a668b7437065610d8aa
Raffael Tancman <raffael.tancman@resultadosdigitais.com.br>
2018-08-19 20:46:20 -0300

    feat: Add mapDispatchToProps and mapStateToProps in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    console.log(this.props)
     return (
       <div>
         <p>Hello world</p>
@@ -12,4 +15,26 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+const mapStateToProps = (calendar) => {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+        ? calendar[day][meal]
+        : null
+        return meals
+      }, {})
+    })),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App) 
