d888411c502fa2a70cb81835e77938382705afcf
Raffael Tancman <raffael.tancman@resultadosdigitais.com.br>
2018-08-19 19:51:15 -0300

    feat: Update App component to use the Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
+import { Provider } from 'react-redux';
 import reducer from './reducers'
 import './index.css';
 import App from './components/App';
@@ -12,5 +13,10 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
